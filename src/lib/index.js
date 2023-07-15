f0957d4c0d731bc870e9cb3315386183d0dffe6a
Andrey Golovin <agamurian@outlook.com>
2023-07-16 00:16:19 +0200

    all questions, some formatting

--- src/lib/index.js
+++ src/lib/index.js
@@ -10,12 +10,12 @@ export const quiz = [
 		type: 'radio',
 		answers: [
 			{
-				id: 0,
+				id: 1,
 				answer: 'Male',
 				weight: -4
 			},
 			{
-				id: 1,
+				id: 2,
 				answer: 'Female',
 				weight: 4
 			}
@@ -27,12 +27,12 @@ export const quiz = [
 		question: 'Do you smoke?',
 		answers: [
 			{
-				id: 0,
+				id: 1,
 				answer: 'No',
 				weight: 0
 			},
 			{
-				id: 1,
+				id: 2,
 				answer: 'Yes',
 				weight: -10
 			}
@@ -44,17 +44,161 @@ export const quiz = [
 		question: 'Do you have any pre-existing diseases?',
 		answers: [
 			{
-				id: 0,
+				id: 1,
 				answer: 'No',
 				weight: 0
 			},
 			{
-				id: 1,
+				id: 2,
 				answer: 'Yes',
 				weight: -10
+			}
+		]
+	},
+	{
+		id: 4,
+		type: 'radio',
+		question: 'What continent do you live in?',
+		answers: [
+			{
+				id: 1,
+				answer: 'North America',
+				weight: 79.5
+			},
+			{
+				id: 2,
+				answer: 'South America',
+				weight: 75.44
+			},
+			{
+				id: 3,
+				answer: 'Australia',
+				weight: 83.2
+			},
+			{
+				id: 4,
+				answer: 'Africa',
+				weight: 64.11
+			},
+			{
+				id: 5,
+				answer: 'Europe',
+				weight: 80.1
+			},
+			{
+				id: 6,
+				answer: 'Asia',
+				weight: 79.2
+			}
+		]
+	},
+	{
+		id: 5,
+		question: 'Do you exercise regularly?',
+		type: 'radio',
+		answers: [
+			{
+				id: 1,
+				answer: 'Yes',
+				weight: 4
+			},
+			{
+				id: 2,
+				answer: 'No',
+				weight: 0
+			}
+		]
+	},
+	{
+		id: 6,
+		question: 'What is your marital status?',
+		type: 'radio',
+		answers: [
+			{
+				id: 1,
+				answer: 'Single',
+				weight: -1
+			},
+			{
+				id: 2,
+				answer: 'Dating',
+				weight: 5
+			},
+			{
+				id: 3,
+				answer: 'Married',
+				weight: 5
+			},
+			{
+				id: 4,
+				answer: 'Divorced',
+				weight: -5
+			},
+			{
+				id: 5,
+				answer: 'Widowed',
+				weight: -5
+			}
+		]
+	},
+	{
+		id: 7,
+		question: 'What is your highest level of education?',
+		type: 'radio',
+		answers: [
+			{
+				id: 1,
+				answer: 'High School',
+				weight: 0
+			},
+			{
+				id: 2,
+				answer: 'Some College',
+				weight: 1
+			},
+			{
+				id: 3,
+				answer: 'Bachelor`s Degree',
+				weight: 2
+			},
+			{
+				id: 4,
+				answer: 'Masters',
+				weight: 3
+			},
+			{
+				id: 5,
+				answer: 'Doctorate',
+				weight: 4
+			}
+		]
+	},
+	{
+		id: 8,
+		question: 'Are you at a healthy weight?',
+		type: 'radio',
+		answers: [
+			{
+				id: 1,
+				answer: 'Yes',
+				weight: 0
+			},
+			{
+				id: 2,
+				answer: 'No',
+				weight: -5
 			}
 		]
 	}
 ];
 
-export const getAge = () => {};
+export const sum = (factors) => {
+	let sum = 0;
+	for (let factor of factors) {
+		sum += factor;
+	}
+	return sum;
+};
+export const getAge = (factors, currentAge) => {
+	return sum(factors) - currentAge;
+};
--- src/routes/quiz/+page.svelte
+++ src/routes/quiz/+page.svelte
@@ -2,7 +2,6 @@
 	import { quiz } from '$lib/index.js';
 	import Modal from '$lib/components/Modal.svelte';
 
-	console.log(quiz.length);
 	let current = 0;
 
 	function clickPrevious() {
@@ -19,27 +18,29 @@
 	$: q = quiz[current];
 </script>
 
-<Modal>
-	<div slot="main">
-		<p class="f-worksans font-big font-600">
-			{q.question}
-		</p>
-		<p>
-			{#if q.type === 'radio'}
-				{#each q.answers as a}
-					<p>{a.answer}</p>
-				{/each}
+<div class="bg">
+	<Modal>
+		<div slot="main">
+			<p class="f-worksans font-big font-600">
+				{q.question}
+			</p>
+			<p>
+				{#if q.type === 'radio'}
+					{#each q.answers as a}
+						<p>{a.answer}</p>
+					{/each}
+				{/if}
+			</p>
+		</div>
+		<div slot="footer-left">
+			{#if current > 0}
+				<button class="btn" on:click={clickPrevious}>previous</button>
 			{/if}
-		</p>
-	</div>
-	<div slot="footer-left">
-		{#if current > 0}
-			<button class="btn" on:click={clickPrevious}>previous</button>
-		{/if}
-	</div>
-	<div slot="footer-right">
-		{#if current < quiz.length - 1}
-			<button class="btn" on:click={clickNext}>next</button>
-		{/if}
-	</div>
-</Modal>
+		</div>
+		<div slot="footer-right">
+			{#if current < quiz.length - 1}
+				<button class="btn" on:click={clickNext}>next</button>
+			{/if}
+		</div>
+	</Modal>
+</div>
